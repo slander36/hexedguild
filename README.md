3982db44076c25a8130714b354c93627c1394592
Sean Lander <s.lander36@gmail.com>
2012-04-30 10:51:01 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Hexed Guild#
+
+Current project for the Hexed Guild website for WoW and TERA.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hexedguild::Application.config.secret_token = '5710ee78f4c33bd63cfe63f6eaf97de6b6e40517a57560b7ba0b75a809ac5e8a42bbe4283b883088041ade2a2ba1e2d035761e87711623202dc816a929f1d52b'
