461ab7e648c46d2850abf8722ac2752f20b20916
Sean Lander <s.lander36@gmail.com>
2012-05-02 16:34:41 -0500

    another cap update

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,24 +25,21 @@ set :deploy_via, :remote_cache
 # Excludes on deploy
 set :copy_exclude, [".git", ".DS_Store", ".gitignore", ".gitmodules"]
 
-
-### This is not being used until zlib stops being not installed on rvm reinstall ###
-
+### Using Capistrano/RVM Gem ###
+#
 # require "rvm/capistrano"
 
-### Using Capistrano/RVM Gem ###
-# set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"") # Read from local system
-# set :rvm_ruby_string, '1.9.3@hexedguild'
+ set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"") # Read from local system
+set :rvm_ruby_string, '1.9.3@hexedguild'
 
 # Tell Capistrano to install rvm, ruby, etc on deploy:setup
-# before 'deploy:setup', 'rvm:install_rvm'
-# before 'deploy:setup', 'rvm:install_ruby'
+before 'deploy:setup', 'rvm:install_rvm'
+before 'deploy:setup', 'rvm:install_ruby'
 
 # Tell Capistrano to update rvm on every deploy
-# before 'deploy', 'rvm:install_rvm'
+before 'deploy', 'rvm:install_rvm'
 
 ### End Capistrano/RVM Gem ###
-
 
 namespace :deploy do
 #  task :start {}
