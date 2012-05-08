743ac5df58f3b739794e068d939bbc6acf9ebff9
Sean Lander <s.lander36@gmail.com>
2012-05-08 15:19:42 -0500

    added images

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,14 @@
 module UsersHelper
+
+	def user_game(user)
+		if (user.wow and user.tera) or (not user.wow and not user.tera)
+			"combined"
+		else
+			if user.wow
+				"wow"
+			else
+				"tera"
+			end
+		end
+	end
 end
